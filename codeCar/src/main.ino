b762ce2440cf03d2f1d259e85396c105b0af0342
unknown <viniciosanhas96@gmail.com>
2021-11-17 00:05:30 -0300

    realiazdo o teste com botao simulando o modulo bluetooth

--- codeCar/src/main.ino
+++ codeCar/src/main.ino
@@ -1,150 +1,177 @@
 #include <Arduino.h>
 
-byte inputs[] = {4, 5, 6, 7, 8, 9, 10, 11};
-byte leds[] = {14, 15, 16, 17};
+byte led = 4;
+byte btn = 13;
 
-void setup() {
+void setup(){
+
+    pinMode(led, OUTPUT);
+    pinMode(btn, INPUT);
+    digitalWrite(led, LOW);
+
+}
+
+void loop(){
+
+    if(digitalRead(btn) == 0){
+
+        digitalWrite(led, HIGH);
+
+    }
+
+    else{
+
+        digitalWrite(led, LOW);
+
+    }
+
+}
+
+// byte inputs[] = {4, 5, 6, 7, 8, 9, 10, 11};
+// byte leds[] = {14, 15, 16, 17};
+
+// void setup() {
   
-  for (int i = 0; i <= sizeof(inputs); i++){
+//   for (int i = 0; i <= sizeof(inputs); i++){
 
-      pinMode(inputs[i], OUTPUT);
-      delay(15);
+//       pinMode(inputs[i], OUTPUT);
+//       delay(15);
 
-  }
+//   }
 
-  for (int i = 0; i <= sizeof(inputs); i++){
+//   for (int i = 0; i <= sizeof(inputs); i++){
 
-      digitalWrite(inputs[i], LOW);
-      delay(15);
+//       digitalWrite(inputs[i], LOW);
+//       delay(15);
 
-  }
+//   }
 
-  for (int i = 0; i <= sizeof(leds); i++){
+//   for (int i = 0; i <= sizeof(leds); i++){
 
-      pinMode(leds[i], OUTPUT);
-      delay(15);
+//       pinMode(leds[i], OUTPUT);
+//       delay(15);
 
-  }
+//   }
 
-  for (int i = 0; i <= sizeof(leds); i++){
+//   for (int i = 0; i <= sizeof(leds); i++){
 
-      digitalWrite(leds[i], LOW);
-      delay(15);
+//       digitalWrite(leds[i], LOW);
+//       delay(15);
 
-  }
+//   }
 
-}
+// }
 
-void loop() {
+// void loop() {
 
-    // INICIO ACELERA
+//     // INICIO ACELERA
 
-    for(int i = 0; i <= sizeof(inputs); i++){
+//     for(int i = 0; i <= sizeof(inputs); i++){
 
-        digitalWrite(inputs[i], LOW);
-        delay(15);
+//         digitalWrite(inputs[i], LOW);
+//         delay(15);
 
-    }
+//     }
 
-    for (int i = 0; i <= sizeof(leds); i++){
+//     for (int i = 0; i <= sizeof(leds); i++){
 
-        digitalWrite(leds[i], LOW);
-        delay(15);
+//         digitalWrite(leds[i], LOW);
+//         delay(15);
 
-    }
+//     }
 
-    digitalWrite(inputs[1], HIGH);
-    digitalWrite(inputs[3], HIGH);
+//     digitalWrite(inputs[1], HIGH);
+//     digitalWrite(inputs[3], HIGH);
 
-    digitalWrite(inputs[5], HIGH);
-    digitalWrite(inputs[7], HIGH);
+//     digitalWrite(inputs[5], HIGH);
+//     digitalWrite(inputs[7], HIGH);
 
-    digitalWrite(leds[0], HIGH);
-    digitalWrite(leds[1], HIGH);
+//     digitalWrite(leds[0], HIGH);
+//     digitalWrite(leds[1], HIGH);
 
-    delay(2000);
+//     delay(2000);
 
-    // FIM ACELERA
+//     // FIM ACELERA
 
-    // INICIO ESQUERDA
+//     // INICIO ESQUERDA
 
-    for(int i = 0; i <= sizeof(inputs); i++){
+//     for(int i = 0; i <= sizeof(inputs); i++){
 
-        digitalWrite(inputs[i], LOW);
-        delay(15);
+//         digitalWrite(inputs[i], LOW);
+//         delay(15);
 
-    }
+//     }
 
-    for (int i = 0; i <= sizeof(leds); i++){
+//     for (int i = 0; i <= sizeof(leds); i++){
 
-        digitalWrite(leds[i], LOW);
-        delay(15);
+//         digitalWrite(leds[i], LOW);
+//         delay(15);
 
-    }
+//     }
 
-    digitalWrite(inputs[1], HIGH);
-    digitalWrite(inputs[3], HIGH);
+//     digitalWrite(inputs[1], HIGH);
+//     digitalWrite(inputs[3], HIGH);
 
-    digitalWrite(leds[0], HIGH);
-    digitalWrite(leds[1], HIGH);
+//     digitalWrite(leds[0], HIGH);
+//     digitalWrite(leds[1], HIGH);
 
-    delay(2000);
+//     delay(2000);
 
-    // FIM ESQUERDA
+//     // FIM ESQUERDA
 
-    // INICIO DIREITA
+//     // INICIO DIREITA
 
-    for(int i = 0; i <= sizeof(inputs); i++){
+//     for(int i = 0; i <= sizeof(inputs); i++){
 
-        digitalWrite(inputs[i], LOW);
-        delay(15);
+//         digitalWrite(inputs[i], LOW);
+//         delay(15);
 
-    }
+//     }
 
-    for (int i = 0; i <= sizeof(leds); i++){
+//     for (int i = 0; i <= sizeof(leds); i++){
 
-        digitalWrite(leds[i], LOW);
-        delay(15);
+//         digitalWrite(leds[i], LOW);
+//         delay(15);
 
-    }
+//     }
 
-    digitalWrite(inputs[4], HIGH);
-    digitalWrite(inputs[6], HIGH);
+//     digitalWrite(inputs[4], HIGH);
+//     digitalWrite(inputs[6], HIGH);
 
-    digitalWrite(leds[0], HIGH);
-    digitalWrite(leds[1], HIGH);
+//     digitalWrite(leds[0], HIGH);
+//     digitalWrite(leds[1], HIGH);
 
-    delay(2000);
+//     delay(2000);
 
-    // FIM DIREITA
+//     // FIM DIREITA
 
-    // INICIA RE
+//     // INICIA RE
     
-    for(int i = 0; i <= sizeof(inputs); i++){
+//     for(int i = 0; i <= sizeof(inputs); i++){
 
-        digitalWrite(inputs[i], LOW);
-        delay(15);
+//         digitalWrite(inputs[i], LOW);
+//         delay(15);
 
-    }
+//     }
 
-    for (int i = 0; i <= sizeof(leds); i++){
+//     for (int i = 0; i <= sizeof(leds); i++){
 
-        digitalWrite(leds[i], LOW);
-        delay(15);
+//         digitalWrite(leds[i], LOW);
+//         delay(15);
 
-    }
+//     }
 
-    digitalWrite(inputs[0], HIGH);
-    digitalWrite(inputs[2], HIGH);
+//     digitalWrite(inputs[0], HIGH);
+//     digitalWrite(inputs[2], HIGH);
 
-    digitalWrite(inputs[4], HIGH);
-    digitalWrite(inputs[6], HIGH);
+//     digitalWrite(inputs[4], HIGH);
+//     digitalWrite(inputs[6], HIGH);
 
-    digitalWrite(leds[2], HIGH);
-    digitalWrite(leds[3], HIGH);
+//     digitalWrite(leds[2], HIGH);
+//     digitalWrite(leds[3], HIGH);
 
-    delay(2000);
+//     delay(2000);
 
-    // FIM RE
+//     // FIM RE
 
-}
+// }
